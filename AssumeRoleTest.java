ff3741865c29a7367a14da7c7e8fd9668d0b8373
anurag1192 <anurag.cav@gmail.com>
2019-03-01 15:55:49 -0600

    Update AssumeRoleTest.java

--- AssumeRoleTest.java
+++ AssumeRoleTest.java
@@ -2,7 +2,7 @@ package com.amazonaws.samples;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicSessionCredentials;
-import com.amazonaws.auth.STSAssumeRoleSessionCredentialsProvider;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.Bucket;
@@ -17,28 +17,28 @@ public class AssumeRoleTest {
 
 	public static void main(String[] args) {
 		
-		GetCallerIdentityRequest callerIdentity = new GetCallerIdentityRequest();
+		// Using default credentials from instance profile. I don't have any credentials in ENV variables or .aws/credentials
 		
-		AWSSecurityTokenService client = AWSSecurityTokenServiceClientBuilder.standard().withRegion("us-east-1").build();
-
-		//The following line is to see which credentials are used to perform AssumeRoleRequest
-				
-		GetCallerIdentityResult res =  client.getCallerIdentity(callerIdentity); 
+		DefaultAWSCredentialsProviderChain creds = new DefaultAWSCredentialsProviderChain();
+		
+		GetCallerIdentityRequest callerIdentity = new GetCallerIdentityRequest();		
+		AWSSecurityTokenService client = AWSSecurityTokenServiceClientBuilder.standard().withCredentials(creds).withRegion("us-east-1").build();		
+		GetCallerIdentityResult res =  client.getCallerIdentity(callerIdentity); //This line is to see which credentials are used to perform AssumeRoleRequest
+		
 		System.out.println("The credentials used to assume role are " + res.getArn());
+		
+		//Replace role arn and role session name 		
 
-		// Using default credentials from my CLI .aws/credentials profile in my CLI
-
-		//Please modify the ROLE ARN and ROLE SESSION NAME below
-		AssumeRoleRequest assume_role = new AssumeRoleRequest().withRoleArn("ROLE-ARN-HERE")
-				.withRoleSessionName("ROLE-SESSION-NAME-HERE");
+		AssumeRoleRequest assume_role = new AssumeRoleRequest().withRoleArn("Role-ARN")
+				.withRoleSessionName("RoleSessionName");
 		
 
 		AssumeRoleResult response = client.assumeRole(assume_role);
 		
 		System.out.println("\n");
-
-		//The following line prints assumed role credentials.
-		//System.out.println(response);
+		//System.out.println(response.getCredentials());
+		
+		
 		// Loading the AssumeRole credentials into sessionCredentials variable below
 		BasicSessionCredentials sessionCredentials = new BasicSessionCredentials(
 				response.getCredentials().getAccessKeyId(), response.getCredentials().getSecretAccessKey(),
@@ -46,26 +46,22 @@ public class AssumeRoleTest {
 		
 
 		// Building the s3 client with the sessionCredentials variable that has AssumeRole Credentials loaded from above
-		// All the subsequent s3 calls are using AssumeRole credentials
+		// All the s3 calls from now are using AssumeRole credentials
 		AmazonS3 s3 = AmazonS3ClientBuilder.standard()
 				.withCredentials(new AWSStaticCredentialsProvider(sessionCredentials)).withRegion("us-east-1").build();
 		
-		//AmazonS3 s3AutomaticRefresh = AmazonS3ClientBuilder.standard()
-        //       .withCredentials(new STSAssumeRoleSessionCredentialsProvider.Builder("ROLE-ARN-HERE", "test").build() ).withRegion("us-east-1")
-        //        .build();
 		
 		//The following stsClientnew is created just to confirm that we are using role credentials to call S3 list buckets operation
 		AWSSecurityTokenService stsClientnew = AWSSecurityTokenServiceClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(sessionCredentials)).withRegion("us-east-1").build();
 		
 
-		GetCallerIdentityResult resu =  stsClientnew.getCallerIdentity(callerIdentity);		
+		GetCallerIdentityResult resu =  stsClientnew.getCallerIdentity(callerIdentity);
+		
 		System.out.println("The credentials used to call s3 requests are " + resu.getArn());
 		
 		System.out.println("\n");
-		//System.out.println(s3);
 
-		
 		System.out.println("Listing buckets");
 		for (Bucket bucket : s3.listBuckets()) {
 			System.out.println(" - " + bucket.getName());
