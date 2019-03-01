ef0a3ee5635a07a4c2fcf81c929cd81a2ff5aa58
anurag1192 <anurag.cav@gmail.com>
2019-03-01 15:59:42 -0600

    Update InstanceCredsTest.java

--- InstanceCredsTest.java
+++ InstanceCredsTest.java
@@ -1,16 +1,24 @@
 package com.amazonaws.samples;
 
-import java.util.concurrent.TimeUnit;
+//import java.util.concurrent.TimeUnit;
 
 import com.amazonaws.auth.AWSCredentialsProviderChain;
 import com.amazonaws.auth.InstanceProfileCredentialsProvider;
+import com.amazonaws.auth.STSAssumeRoleSessionCredentialsProvider;
 import com.amazonaws.services.identitymanagement.AmazonIdentityManagement;
 import com.amazonaws.services.identitymanagement.AmazonIdentityManagementClientBuilder;
 import com.amazonaws.services.identitymanagement.model.GetUserRequest;
 import com.amazonaws.services.identitymanagement.model.GetUserResult;
+//import com.amazonaws.services.s3.AmazonS3;
+//import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+//import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.securitytoken.AWSSecurityTokenService;
+import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClientBuilder;
+import com.amazonaws.services.securitytoken.model.GetCallerIdentityRequest;
+import com.amazonaws.services.securitytoken.model.GetCallerIdentityResult;
 
 public class InstanceCredsTest {
 
@@ -18,27 +26,35 @@ public class InstanceCredsTest {
 		
 		AWSCredentialsProviderChain creds = new AWSCredentialsProviderChain(new InstanceProfileCredentialsProvider(true));
 		
-		
-		AmazonS3 s3 = AmazonS3ClientBuilder.standard()
-	              .withCredentials(new InstanceProfileCredentialsProvider(false))
-	              .build();
-		
-		
 		AmazonIdentityManagement iamautomatic = AmazonIdentityManagementClientBuilder.standard()
 	               .withCredentials(creds).withRegion("us-east-1")
 	               .build();
 		
-		for(int i=0; i< 10; i++) {
+
 			
 			GetUserRequest request = new GetUserRequest().withUserName("Test");
 			GetUserResult response = iamautomatic.getUser(request);
 			System.out.println("Test user id is " + response.getUser().getUserId() + " - Access Key used is " + creds.getCredentials().getAWSAccessKeyId());
 			
-		}
-		
 
+		System.out.println();
+		
+		// Uses STS assume role credentials provider that has the feature to refresh assumed role credentials automatically
+		// Change role arn and role session name
+		STSAssumeRoleSessionCredentialsProvider creds1 = new STSAssumeRoleSessionCredentialsProvider.Builder("Role-ARN", "RoleSessionName").build();
+		
+		GetCallerIdentityRequest callerIdentity = new GetCallerIdentityRequest();		
+		AWSSecurityTokenService client = AWSSecurityTokenServiceClientBuilder.standard().withCredentials(creds1).withRegion("us-east-1").build();
+		GetCallerIdentityResult res =  client.getCallerIdentity(callerIdentity);
+		
+		System.out.println("Credentials from STSAssumeRoleSessionCredentialsProvider are : " + res.getArn());
+		
+		AmazonS3 s3AutomaticRefresh = AmazonS3ClientBuilder.standard()
+	               .withCredentials(creds1).withRegion("us-east-1")
+	                .build();
+		
 		System.out.println("Listing buckets");
-		for (Bucket bucket : s3.listBuckets()) {
+		for (Bucket bucket : s3AutomaticRefresh.listBuckets()) {
 			System.out.println(" - " + bucket.getName());
 		}
 		System.out.println();
